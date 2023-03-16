e2941fafa8c8469263aaa3d4d5fe0e42917b530d
Ajay Bharadwaj <47782857+AnorexicAtticusFinch@users.noreply.github.com>
2023-03-16 18:49:09 -0400

    Add files via upload

--- backend/lf2.py
+++ backend/lf2.py
@@ -1,2 +1,10 @@
 def main(event, context):
     print("LF2")
+
+    return {
+        "statusCode": 200,
+        "body": "{'Test': 'Test'}",
+        "headers": {
+            'Content-Type': 'application/json',
+        }
+    }
