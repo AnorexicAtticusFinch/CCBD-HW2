5a822f90cd12af0bf572e8832831a14a592df277
shubham050300 <shubham050300@gmail.com>
2023-03-18 16:48:43 -0400

    frontend

--- backend/lf2.py
+++ backend/lf2.py
@@ -1,5 +1,19 @@
-def main(event, context):
+import json
+import datetime
+import boto3
+
+def lambda_handler(event, context):
     print("LF2")
+
+    photos = []
+
+    txt = event['messages'][0]['query']
+    print("txt", txt)
+
+    client = boto3.client("lexv2-runtime")
+    response = client.recognize_text(botId='ERUDNYDIAN', botAliasId='USVXTEBHZN', localeId='en_US', sessionId='test_session', text=txt)
+    print("RESP", response)
+
 
     return {
         "statusCode": 200,
