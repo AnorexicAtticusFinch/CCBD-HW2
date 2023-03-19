d969f8d286c87e6a6c471d385ca4bd650c055f91
shubham050300 <shubham050300@gmail.com>
2023-03-18 23:40:17 -0400

    lambda search updated

--- backend/lf2.py
+++ backend/lf2.py
@@ -1,23 +1,50 @@
 import json
 import datetime
 import boto3
+import os
 
 def main(event, context):
     print("LF2")
 
     photos = []
 
-    txt = event['messages'][0]['query']
+    txt = event['q']
     print("txt", txt)
 
     client = boto3.client("lexv2-runtime")
     response = client.recognize_text(botId='ERUDNYDIAN', botAliasId='USVXTEBHZN', localeId='en_US', sessionId='test_session', text=txt)
-    print("RESP", response)
+    print("RESP", response['sessionState']['intent']['slots'])
+    response_slots = response['sessionState']['intent']['slots']
 
+    query = []
+    if response_slots['query_term1'] != None:
+        query.append(response_slots['query_term1']['value']['interpretedValue'])
+    if response_slots['query_term2'] != None:
+        query.append(response_slots['query_term2']['value']['interpretedValue'])
+
+
+    #OpenSearch query
+    host = os.environ.get('OPENSEARCH_LINK')
+    index = 'photos'
+    url = host + '/' + index + '/' + '_search/'
+    headers = { "Content-Type": "application/json", "Authorization": "Basic bWFzdGVyVXNlckNDQkRIVzI6SUxpa2VCaWdNdXR0c0FuZElDYW5ub3RMMTMh" }
+
+    for q in query:
+        r = requests.get(url + '?=' + q, headers=headers)
+        r.raise_for_status()
+        body = r.json()
+        items = body["hits"]["hits"]
+        for item in items:
+            bucket = item["_source"]["bucket"]
+            key = item["_source"]["objectKey"]
+            photoURL = "https://{0}.s3.amazonaws.com/{1}".format(bucket,key)
+            photos.append(photoURL)        
+
+    print("Photos count: ", len(photos))
 
     return {
         "statusCode": 200,
-        "body": "{'Test': 'Test'}",
+        "body": json.dumps(photos),
         "headers": {
             'Content-Type': 'application/json',
         }
